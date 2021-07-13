f46f344b5c22bff9630be5f6c6e409d09559e122
Itamar Turner-Trauring <itamar@itamarst.org>
2021-07-13 09:27:05 -0700

    File was moved

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # macOS on Conda is ancient so doesn't have MAP_ANONYMOUS:
-sed -i -e 's/MAP_ANONYMOUS/MAP_ANON/' filprofiler/_filpreload.c
+sed -i -e 's/MAP_ANONYMOUS/MAP_ANON/' filpreload/src/_filpreload.c
 
 # Build and install Python code:
 export PIP_LOG=/dev/stdout
