ab759220c7463caa1b9d526e3aec0ea248a4ccf6
Itamar Turner-Trauring <itamar@itamarst.org>
2021-11-10 06:43:08 -0500

    Try using lld instead of gold.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 # macOS on Conda is ancient so doesn't have MAP_ANONYMOUS:
 sed -i -e 's/MAP_ANONYMOUS/MAP_ANON/' filpreload/src/_filpreload.c
 
+# Maybe gold is bad?
+sed -i -e 's/use-ld=gold/use-ld=lld/' filpreload/build.rs
+
 # Build and install Python code:
 export PIP_LOG=/dev/stdout
 $PYTHON -m pip install . -vv
